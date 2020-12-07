5747ab70dc30f23acab96342b4841fed44ebc2b5
calebwkang <caleb.kang@unc.edu>
2020-12-07 15:04:13 -0500

    xcode proj and tab bar controller

---
+++ moodchart/moodchart/SocialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SocialViewController.swift
+//  moodchart
+//
+//  Created by Caleb Kang on 12/7/20.
+//
+
+import UIKit
+
+class SocialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
